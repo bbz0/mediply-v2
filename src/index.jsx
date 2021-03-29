ca918cb9963ad5ab45f44471afcde10e3c2415a4
zach <zachariahmanabat@gmail.com>
2021-03-29 20:29:02 +0800

    spot fixes

--- src/index.jsx
+++ src/index.jsx
@@ -1,6 +1,6 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import { BrowserRouter, Route, Switch, Redirect } from 'react-router-dom';
+import { BrowserRouter, HashRouter, Route, Switch, Redirect } from 'react-router-dom';
 
 import './index.css';
 
--- src/views/Landing.jsx
+++ src/views/Landing.jsx
@@ -12,7 +12,7 @@ class Product extends React.Component {
   render() {
     const products = this.state.products.map((product, i) => {
       return (
-        <div className="w-full mb-6 border-2 border-blue rounded-lg shadow-lg">
+        <div className="w-full mb-6 border-2 border-blue rounded-lg shadow-lg" key={product.name}>
           <img className="mx-auto my-6 h-60 px-6" src={product.file} alt={product.name} />
           <div className="p-4 h-32 bg-blue">
             <h5 className="text-2xl text-blue-100 font-extrabold">{product.name}</h5>
@@ -53,7 +53,7 @@ export default function Landing() {
         <div className="container mx-auto px-6 py-6">
           <h4 className="mb-6 text-2xl text-blue-100 font-extrabold">Contact Us</h4>
 
-          <div class="flex flex-wrap">
+          <div className="flex flex-wrap">
             <div className="mb-6 w-full sm:w-1/2 lg:w-1/3">
               <h6 className="text-md text-blue-100 font-medium">Address</h6>
               <span className="text-md text-blue-100 font-bold">20 Empress Road, Bgy. San Isidro <br />Antipolo City</span>
